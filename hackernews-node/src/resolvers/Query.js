b0c86518cc34b788279c310096ce3b3464a0b390
haglermgp <hagler.mijail@gmail.com>
2018-01-31 12:11:20 -0500

    [add] First started whith configuration graphql

---
+++ hackernews-node/src/resolvers/Query.js
@@ -0,0 +1,13 @@
+function feed (parent, args, context, info) {
+  const { filter, first, skip } = args // restructure input arguments
+
+  const where = filter
+		? { OR: [{ url_contains: filter }, { description_contains: filter }] }
+		: {}
+
+  return context.db.query.links({ first, skip, where }, info)
+}
+
+module.exports = {
+  feed
+}
